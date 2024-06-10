de0de6005daa6b5b1ff89b3ec666397cbd5a9e25
madhu2904 <madhumithavelusamy0@outlook.com>
2024-06-10 10:24:15 +0530

    Final commit of changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [to_do_list,setTodoList]=useState([])
+  const [newTask,setNewTask]=useState("")
+
+  // to create a listener of input task
+  const handle_new_task=(event)=>{
+      setNewTask(event.target.value);
+  }
+
+  const add_task=()=>{
+    setTodoList([...to_do_list,newTask])
+  }
+
+  const delete_task=(taskName)=>
+    {
+      const newList=to_do_list.filter((task)=>
+      {
+        return taskName !== task
+      });
+      setTodoList(newList);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="add_task">
+      {/* new task input box  */}
+      <input onChange={handle_new_task}/>
+      <button onClick={add_task}>Add Task</button>
+     </div>
+
+     <div className='task_list'>
+      {
+      to_do_list.map((task,key)=>
+        {
+               return <div>
+                <h1>{task}</h1><button onClick={
+                   ()=>
+                   {
+                     setTodoList([to_do_list.filter((innertask) => {return innertask !== task})])
+                   }
+                  // ()=>delete_task(task)
+                }
+                  >remove/done</button>
+                </div>;
+      })
+      }
+     </div>
     </div>
   );
 }
 
 export default App;
+
+
+
